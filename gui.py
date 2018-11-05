2143cc23fc17f2557bd07512ccfa69753952766e
“Yannick” <yannick93@Live.nl>
2018-11-05 20:36:33 +0100

    GUI werkend met classes

--- gui.py
+++ gui.py
@@ -6,122 +6,174 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
-
-style.use('ggplot')
-
-root = Tk()                #aanmaken van venster
-root.title('Python User Interface')
-
-main = Frame(root)
-main.pack(side=TOP)
-
-main2 = Frame(root)
-main2.pack()
+from test import data
 
 
-Label1 = Label(main2, text='IT-Works', fg='black')
-Label1.grid(row=0,column=1, columnspan= 4)
+class Program:
 
-def uitrollen():
-    status = 0
-    if (status == 0):
-        Label6 = Label(main2, text='uitgerold', fg='red')
-        Label6.grid(row=1, column=5, columnspan=1)
+    main = Frame(bg='grey')
+    main.pack(side=TOP)
 
-def inrollen():
-    status = 1
-    if (status == 1):
-        Label6 = Label(main2, text='ingerold', fg='green')
-        Label6.grid(row=1, column=5, columnspan=1)
-
-Label5 = Label(main2, text='Status: ', fg='black')
-Label5.grid(row=1,column=4, columnspan= 1)
-
-button1 = Button(main2,width=10, height=2, text="Uitrollen", fg="black", command=uitrollen)
-button1.grid(row=1,column=2)
-
-button2 = Button(main2,width=10, height=2, text="Inrollen", fg="black", command=inrollen)
-button2.grid(row=1,column=3)
-
-Label2 = Label(main2, text='Temperatuur \n 0 tot 100 celcius', fg='black')
-Label2.grid(row=2,column=0)
-
-temperatuur = Scale(main2, from_=100, to=0)
-temperatuur.grid(row=3,column=0)
-
-Label3 = Label(main2, text='Lichtintensiteit \n 0 tot 100', fg='black')
-Label3.grid(row=2,column=1)
-
-licht = Scale(main2, from_=100, to=0)
-licht.grid(row=3,column=1)
-
-Label4 = Label(main2, text='Lichtintensiteit', fg='black')
-Label4.grid(row=2,column=2,columnspan=2)
-
-Label4 = Label(main2, text='Temperatuur', fg='black')
-Label4.grid(row=2,column=4,columnspan=2)
-
-button3 = Button(main2, text="EXIT", fg="black",width=10, height=2, command=quit)
-button3.grid(row=4,column=1,columnspan=4)
+    def __init__(self):
 
 
-x=list()
-y=list()
+        self.Label1 = Label(self.main, text='Temperatuur', fg='black', bg = 'grey')
+        self.Label1.grid(row=0, column=0, columnspan=4)
+        self.Label2 = Label(self.main, text='Huidige temperatuur: ', fg='black', bg='grey')
+        self.Label2.grid(row=1, column=0, columnspan=2)
+        self.Label4 = Label(self.main, text='Grens Temperatuur: ', fg='black', bg='grey')
+        self.Label4.grid(row=2, column=0, columnspan=2)
+        self.Label5 = Label(self.main, text='0', fg='black', bg='grey')
+        self.Label5.grid(row=1, column=2, padx=50)
+        self.temperatuur = Scale(self.main, orient='horizontal', from_=0, to=100, length=200, command= self.temperatuur)
+        self.temperatuur.grid(row=4, column=1, columnspan=2)
 
 
-for i in range(12,23):
-    x.append(i)
 
-for i in range(1,12):
-    y.append(i)
+        self.Label10 = Label(self.main, text='Licht intensiteit', fg='black', bg='grey')
+        self.Label10.grid(row=0, column=4, columnspan=2)
+        self.Label12 = Label(self.main, text='Huidige licht intensiteit: ', fg='black', bg='grey')
+        self.Label12.grid(row=1, column=3, columnspan=2, padx=50)
+        self.Label14 = Label(self.main, text='Grens licht intensiteit: ', fg='black', bg='grey')
+        self.Label14.grid(row=2, column=3, columnspan=2)
+        self.Label15 = Label(self.main, text='0', fg='black', bg='grey')
+        self.Label15.grid(row=1, column=5, padx=50)
+        self.licht = Scale(self.main, orient='horizontal', from_=0, to=100, length=200, command= self.licht)
+        self.licht.grid(row=4, column=4, columnspan=2)
 
-def plot_graph():
-    figure = Figure(figsize=(4,4), dpi=70)
-
-    a = figure.add_subplot(111)
-    a.plot(x,y,marker='o')
-    a.grid()
-
-    canvas = FigureCanvasTkAgg(figure, master=main2)
-    canvas.draw()
-
-    graph_widget= canvas.get_tk_widget()
-    graph_widget.grid(row=3,column=2, columnspan=2,sticky='nsew')
+        self.Label16 = Label(self.main, text='Status', fg='black', bg='grey')
+        self.Label16.grid(row=0, column=8, columnspan=2)
+        self.button = Button(self.main, width=10, height=2, text="Uitrollen", fg="black", command=self.uitrollen)
+        self.button.grid(row=1, column=8)
+        self.button2 = Button(self.main, width=10, height=2, text="Inrollen", fg="black", command=self.inrollen)
+        self.button2.grid(row=1, column=9)
 
 
-xx=list()
-yy=list()
+        self.Label17 = Label(self.main, text='Temperatuur', fg='black', bg = 'grey')
+        self.Label17.grid(row=5, column=0, columnspan=4, pady=30)
+        self.show_graph()
 
-for i in range(12,23):
-    xx.append(i)
+        self.Label18 = Label(self.main, text='Licht intensiteit', fg='black', bg = 'grey')
+        self.Label18.grid(row=5, column=4, columnspan=4, pady=30)
+        self.show_graph2()
 
-for i in range(1,12):
-    yy.append(i)
+        self.Label19 = Label(self.main, text='Derde grafiek', fg='black', bg = 'grey')
+        self.Label19.grid(row=5, column=8, columnspan=4, pady=30)
+        self.show_graph3()
 
-def plot_graph2():
-    figure1 = Figure(figsize=(4,4), dpi=70)
+        self.Label20 = Label(self.main, text='Vierde grafiek', fg='black', bg = 'grey')
+        self.Label20.grid(row=7, column=0, columnspan=4, pady=30)
+        self.show_graph4()
 
-    aa = figure1.add_subplot(111)
-    aa.plot(xx,yy,marker='o')
-    aa.grid()
+        self.Label21 = Label(self.main, text='Vijfde grafiek', fg='black', bg = 'grey')
+        self.Label21.grid(row=7, column=4, columnspan=4, pady=30)
+        self.show_graph5()
 
-    canvas1 = FigureCanvasTkAgg(figure1, master=main2)
-    canvas1.draw()
+    def temperatuur(self, value):
+        self.Label3 = Label(self.main, text=value, fg='black', bg='grey')
+        self.Label3.grid(row=2, column=2)
 
-    graph_widget1 = canvas1.get_tk_widget()
-    graph_widget1.grid(row=3,column=4, columnspan=3,sticky='nsew')
+    def licht(self, value):
+        self.Label13 = Label(self.main, text=value, fg='black', bg='grey')
+        self.Label13.grid(row=2, column=5)
 
-plot_graph()
-plot_graph2()
+    def inrollen(self):
+        status = 1
+        if (status == 1):
+            Label22 = Label(self.main, text='ingerold', fg='red', bg='grey')
+            Label22.grid(row=4, column=8, columnspan=2)
 
-def grafiek():
-    a=24
-    b=16
-    x.append(a)
-    y.append(b)
-    plot_graph()
+    def uitrollen(self):
+        status = 0
+        if (status == 0):
+            Label22 = Label(self.main, text='uitgerold', fg='green', bg='grey')
+            Label22.grid(row=4, column=8, columnspan=2)
 
-button3 = Button(main2, text="GRAFIEK", fg="black",width=10, height=2, command=grafiek)
-button3.grid(row=4,column=2,columnspan=1)
+    def show_graph(self):
 
-root.mainloop()
+        self.x = data.listx
+        self.y = data.listy
+
+        figure = Figure(figsize=(4,4), dpi=70)
+
+        a = figure.add_subplot(111)
+        a.plot(self.x,self.y,marker='o')
+        a.grid()
+
+        canvas = FigureCanvasTkAgg(figure, master=self.main)
+        canvas.draw()
+
+        graph_widget= canvas.get_tk_widget()
+        graph_widget.grid(row=6,column=1,columnspan=2, padx=80, sticky='nsew')
+
+    def show_graph2(self):
+
+        self.x = data.listx
+        self.y = data.listy
+
+        figure1 = Figure(figsize=(4, 4), dpi=70)
+
+        aa = figure1.add_subplot(111)
+        aa.plot(self.x, self.y, marker='o')
+        aa.grid()
+
+        canvas1 = FigureCanvasTkAgg(figure1, master=self.main)
+        canvas1.draw()
+
+        graph_widget1 = canvas1.get_tk_widget()
+        graph_widget1.grid(row=6, column=4, columnspan=2,sticky='nsew', padx=80)
+
+    def show_graph3(self):
+
+        self.x = data.listx
+        self.y = data.listy
+
+        figure1 = Figure(figsize=(4, 4), dpi=70)
+
+        aa = figure1.add_subplot(111)
+        aa.plot(self.x, self.y, marker='o')
+        aa.grid()
+
+        canvas1 = FigureCanvasTkAgg(figure1, master=self.main)
+        canvas1.draw()
+
+        graph_widget1 = canvas1.get_tk_widget()
+        graph_widget1.grid(row=6, column=8, columnspan=3,sticky='nsew', padx=80)
+
+    def show_graph4(self):
+
+        self.x = data.listx
+        self.y = data.listy
+
+        figure1 = Figure(figsize=(4, 4), dpi=70)
+
+        aa = figure1.add_subplot(111)
+        aa.plot(self.x, self.y, marker='o')
+        aa.grid()
+
+        canvas1 = FigureCanvasTkAgg(figure1, master=self.main)
+        canvas1.draw()
+
+        graph_widget1 = canvas1.get_tk_widget()
+        graph_widget1.grid(row=8, column=1, columnspan=2,sticky='nsew', padx=80)
+
+    def show_graph5(self):
+
+        self.x = data.listx
+        self.y = data.listy
+
+        figure1 = Figure(figsize=(4, 4), dpi=70)
+
+        aa = figure1.add_subplot(111)
+        aa.plot(self.x, self.y, marker='o')
+        aa.grid()
+
+        canvas1 = FigureCanvasTkAgg(figure1, master=self.main)
+        canvas1.draw()
+
+        graph_widget1 = canvas1.get_tk_widget()
+        graph_widget1.grid(row=8, column=4, columnspan=3,sticky='nsew', padx=80)
+
+program = Program()
+
+mainloop()
