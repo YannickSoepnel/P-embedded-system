545c584efe366679be9b278984a65fba519af0bd
“Yannick” <yannick93@Live.nl>
2018-11-06 11:47:03 +0100

    Test data werkt nu met GUI

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
+class data:
+    list1 = [(0, 10), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
+    list2 = [(0, 2), (1, 3), (2, 4), (3, 5), (4, 6), (5, 7), (6, 8), (7, 9), (8, 10)]
+    listx = []
+    listy = []
+    count = 0
+    for i in list1:            #x waarden
+        if(len(list1) > count):
+            listx.append(list1[count][0])
+            count+=1
+    count2 = 0
+    for i in list2:            #y waarden
+        if(len(list2) > count2):
+            listy.append(list1[count2][1])
+            count2+=1
 
-
-list3 = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
-list4 = [(0, 2), (1, 3), (2, 4), (3, 5), (4, 6), (5, 7), (6, 8), (7, 9), (8, 10)]
+    f = open("input_data.txt", "w")
+    while True:
+        for x in listx:
+            f.write(str(x))
