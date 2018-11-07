59aeccb1ed8762913bc13e791a73fd50915b7735
Jarco Uil <jarcouil@gmail.com>
2018-11-07 10:17:48 +0100

    lichtintesniteit project toegevoegd
    lichtintesniteit project toegevoegd

---
+++ atmel studio/lichtintensiteit/lichtintensiteisensor/lichtintensiteisensor/main.c
@@ -0,0 +1,18 @@
+/*
+ * lichtintensiteisensor.c
+ *
+ * Created: 7-11-2018 10:15:43
+ * Author : jarco
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
