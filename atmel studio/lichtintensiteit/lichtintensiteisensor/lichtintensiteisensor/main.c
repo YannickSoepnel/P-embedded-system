1baa0628e481818386b785560d6f0530bba81e1b
Jarco Uil <jarcouil@gmail.com>
2018-11-13 12:46:20 +0100

    main.c heeft inhoud

--- atmel studio/lichtintensiteit/lichtintensiteisensor/lichtintensiteisensor/main.c
+++ atmel studio/lichtintensiteit/lichtintensiteisensor/lichtintensiteisensor/main.c
@@ -6,13 +6,54 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16E6
+#include <stdlib.h>
+#include <avr/sfr_defs.h>
+#include <util/delay.h>
+#define UBBRVAL 51
 
+uint8_t data;
+
+void uart_init()
+{
+	// set the baud rate
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;     	// disable U2X mode
+	UCSR0A = 0;		        // enable transmitter
+	UCSR0B = _BV(TXEN0); 	// enable receiver
+	UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+	// set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+
+void transmit(uint8_t data)
+{
+	// wait for an empty transmit buffer
+	// UDRE is set when the transmit buffer is empty
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	// send the data
+	UDR0 = data;
+}
+
+uint8_t readlight()
+{
+	uint8_t value;
+	value = PINC;
+	return value;
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRC = 0x00;
+    uart_init();
     while (1) 
     {
+		PORTC = 0;
+		_delay_ms(50);
+		data = readlight();
+		transmit(data);
+			
     }
 }
 
